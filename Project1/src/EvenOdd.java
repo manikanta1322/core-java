f6b1c1c1a2a517192e4a199d6cdcaa8c0c5d6726
DELL <DELL@ManikantaChemiti>
2022-05-06 21:32:26 +0530

    First Commit

--- Project1/src/EvenOdd.java
+++ Project1/src/EvenOdd.java
@@ -1,8 +1,17 @@
-Scanner import
+import java.util.Scanner;
+
 public class EvenOdd {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Enter a number: ");
+		int num = reader.nextInt();
+		
+		String EvenOdd = (num % 2 == 0) ? "Even" : "Odd";
+		
+		System.out.println(num + " is " + EvenOdd);
 
 	}
 
